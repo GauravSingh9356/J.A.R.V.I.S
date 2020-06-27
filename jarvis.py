19fd02d8f6a0f8b2208c80fb443c3bab18d44366
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-06-27 09:54:27 +0530

    1.0.0

---
+++ jarvis.py
@@ -0,0 +1,113 @@
+import pyttsx3
+import wikipedia
+import speech_recognition as sr
+import webbrowser
+import datetime
+import os
+import sys
+import smtplib
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+# print(voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.energy_threshold = 494
+        r.adjust_for_ambient_noise(source, duration=1.5)
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing..')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'User said: {query}\n')
+
+    except Exception as e:
+        # print(e)
+
+        print('Say that again please...')
+        return 'None'
+    return query
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning SIR")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon SIR")
+
+    else:
+        speak('Good Evening! SIR')
+    speak('I am JARVIS. Please tell me how can I help you SIR?')
+
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('gs935688@gmail.com', 'spectrum12345#')
+    server.sendmail('gs935688email@gmail.com', to, content)
+    server.close()
+
+
+if __name__ == '__main__':
+    chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+    webbrowser.register(
+        'chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia....')
+            query = query.replace('wikipedia', '')
+            results = wikipedia.summary(query, sentences=2)
+            speak('According to Wikipedia')
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+
+            webbrowser.get('chrome').open_new_tab('https://youtube.com')
+        elif 'open google' in query:
+            webbrowser.get('chrome').open_new_tab('https://google.com')
+
+        elif 'open stackoverflow' in query:
+            webbrowser.get('chrome').open_new_tab('https://stackoverflow.com')
+        elif 'play music' in query:
+            os.startfile("D:\\RoiNa.mp3")
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f'Sir, the time is {strTime}')
+
+        elif 'open code' in query:
+            os.startfile(
+                "C:\\Users\\gs935\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+        elif 'shutdown' in query:
+            os.system('shutdown /p /f')
+        elif 'github' in query:
+            webbrowser.get('chrome').open_new_tab(
+                'https://github.com/gauravsingh9356')
+
+        elif 'sleep' in query:
+            sys.exit()
+        elif 'email to gaurav' in query:
+            try:
+                speak('What should I say?')
+                content = takeCommand()
+                to = 'gs935688@gmail.com'
+                sendEmail(to, content)
+                speak('Email has been sent!')
+
+            except Exception as e:
+                speak('Sorry sir, Not able to send email at the moment')
