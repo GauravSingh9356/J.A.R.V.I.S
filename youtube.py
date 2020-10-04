06bf74a2088c37e84572a393ff5d8c422f75756e
nsankethreddy <sankethn1947@gmail.com>
2020-10-04 21:01:20 +0530

    Add multiOS support to youtube.py

--- youtube.py
+++ youtube.py
@@ -1,26 +1,29 @@
 from urllib.request import urlopen
 import urllib.parse
-import re
-from bs4 import BeautifulSoup
 import webbrowser
-chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-webbrowser.register(
-    'chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+from sys import platform
+import os
+
+if platform == "linux" or platform == "linux2":
+    chrome_path = '/usr/bin/google-chrome'
+
+elif platform == "darwin":
+    chrome_path = 'open -a /Applications/Google\ Chrome.app'
+
+elif platform == "win32":
+    chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+else:
+    print('Unsupported OS')
+    exit(1)
+
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 
 
-def you(textToSearch):
+def youtube(textToSearch):
     query = urllib.parse.quote(textToSearch)
     url = "https://www.youtube.com/results?search_query=" + query
     webbrowser.get('chrome').open_new_tab(url)
-    #response = urlopen(url)
-    #html = response.read()
-    #soup = BeautifulSoup(html, "lxml")
-    #flag = 0
-    #search_results=re.findall('href=\"\\/watch\\?v=(.{11})', html.decode())
-    #print(search_results)
-    #webbrowser.get('chrome').open_new_tab('http://youtube.com/watch?v=' + search_results[0])
-    
 
 
 if __name__ == '__main__':
-    you('any text')
+    youtube('any text')
